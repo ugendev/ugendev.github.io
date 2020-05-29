b622d31775205eaa624f1b2e9fd4f383d4cc48b0
Eugene <katasonov-jenyaa@mail.ru>
2020-05-30 00:44:19 +0300

    added js animation to 'testimonials' section

---
+++ src/blocks/testimonials/__slider/testimonials__slider.js
@@ -0,0 +1,36 @@
+function slider() {
+    const
+        slidesWrapper   = document.querySelector(".testimonials__slider"),  
+        slides          = slidesWrapper.querySelectorAll("li"),
+
+        dots            = slidesWrapper.parentElement.querySelector(".testimonials__slider-dots"),
+        dot             = dots.querySelectorAll("li"),
+        dotsCount       = dot.length;
+
+    let 
+        slideWidth      = slides[0].clientWidth,
+        wrapperWidth    = slideWidth * dotsCount,
+
+        current         = 0,
+        target;
+
+    slidesWrapper.style.cssText = `width: ${wrapperWidth}px;`;
+        
+    dots.addEventListener("click", function(event) {
+        target = event.target;
+
+        for (let i = 0; i < dotsCount; i++) {
+            let temp = dot[i];
+
+            if (temp == target) {
+                slidesWrapper.style.cssText += `transform: translateX(-${slideWidth * i}px);`;
+                dot[current].classList.remove("testimonials__slider-dot_active");
+                dot[i].classList.add("testimonials__slider-dot_active");
+                current = i;
+            }
+        }
+    });
+
+}
+
+module.exports = slider;
--- src/js/main.js
+++ src/js/main.js
@@ -4,10 +4,12 @@ require('formdata-polyfill');
 window.addEventListener("DOMContentLoaded", function() {
 
     const rotateCards       = require("../blocks/portfolio/__items/portfolio__items"),
-          switchContent     = require("../blocks/our-team/__cards/our-team__cards"); 
+          switchContent     = require("../blocks/our-team/__cards/our-team__cards"),
+          slider            = require("../blocks/testimonials/__slider/testimonials__slider");
 
     rotateCards();
     switchContent();
+    slider();
 });
 
 
